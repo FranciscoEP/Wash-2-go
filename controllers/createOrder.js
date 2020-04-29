cc3727b87d463c1ec7cb629445684474c8f40042
FranciscoEP <francisco.eugp@gmail.com>
2020-04-29 12:03:27 -0500

    sharing Update CRUD

--- controllers/createOrder.js
+++ controllers/createOrder.js
@@ -4,12 +4,23 @@ const Sector = require('../models/Sector')
 
 exports.detailOrderView = (req, res) => {
   const { _id: userId } = req.user
-  Sector.find({userId})
-  .then(theOrders => {
-    console.log(theOrders)
-    res.render('auth/detailOrder', {order: theOrders})
-  })
-  .catch(error => {
-    console.log('Error while retrieving book details: ', error);
-  })
+  Sector.find({ userId })
+    .then((theOrders) => {
+      res.render('auth/detailOrder', { order: theOrders })
+    })
+    .catch((error) => {
+      console.log('Error while retrieving book details: ', error)
+    })
 }
+
+exports.editOrderView = async (req, res) => {
+  const edit = req.params.id
+  const editOrder = await Sector.findById(edit)
+  res.render('auth/editOrder', edit)
+}
+
+// exports.editOrderAdd = async (req, res) => {
+//   const edit = req.params.id
+//   await Sector.findByIdAndUpdate(edit, { $set: { ...req.body } }, { new: true })
+//   res.render(`auth/editOrder/${edit}`, edit)
+// }
--- routes/createOrder.js
+++ routes/createOrder.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login')
-const { detailOrderView } = require('../controllers/createOrder')
+const { editOrderView, /*editOrderAdd, */ detailOrderView } = require('../controllers/createOrder')
 
 //Read
 router.get('/detailOrder', ensureLoggedIn(), detailOrderView)
+
+//Update
+router.get('/editOrder/:id', ensureLoggedIn(), editOrderView)
+// router.post('/editOrder/:id', ensureLoggedIn(), editOrderAdd)
 
 module.exports = router
